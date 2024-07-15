9a903f37890834e99fff745e68b5fe18c15044e3
Emmanuel <manuelckay@gmail.com>
2024-07-15 07:44:34 +0100

    Corrected &apos; apostrophe error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,7 +13,7 @@ const NotFound = () => {
         <div className="text-center md:text-left">
           {/* <h1 className="text-3xl font-bold text-gray-800">404 - Page Not Found</h1> */}
           <p className="text-gray-500 mt-4">
-            The page you're looking for might have been removed or never existed.
+            The page you&apos;re looking for might have been removed or never existed.
           </p>
           <Link href="/">
             <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
